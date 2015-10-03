51c2d5578a3a921546020802531e30d92e980a6e
Russell Parker <russell.parker7@gmail.com>
2015-10-03 17:22:37 -0400

    Files from beareded google guy

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask.ext.googlemaps import GoogleMaps
+from flask.ext.googlemaps import Map
+
+app = Flask(__name__, template_folder=".")
+GoogleMaps(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def mapview():
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers={'http://maps.google.com/mapfiles/ms/icons/green-dot.png':[(37.4419, -122.1419)],
+                 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png':[(37.4300, -122.1400)]}
+    )
+    return render_template('example.html', mymap=mymap, sndmap=sndmap)
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
