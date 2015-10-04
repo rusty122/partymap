e92a3b2d5267cb23f8cef0b483024db5a1847a51
ian-bateman <ian.h.bateman.18@dartmouth.edu>
2015-10-04 08:24:28 -0400

    Changed map styling for Google Map

--- static/js/initmap.js
+++ static/js/initmap.js
@@ -1,10 +1,44 @@
 function initialize() {
-    var mapCanvas = document.getElementById('map');
+
+  	var customMapType = new google.maps.StyledMapType([
+	      {
+	        stylers: [
+	          {hue: '#5fade7'},
+	          {visibility: 'simplified'},
+	          {gamma: 0.5},
+	          {weight: 0.5}
+	        ]
+	      },
+	      {
+	        elementType: 'labels',
+	        stylers: [{visibility: 'off'}]
+	      },
+	      {
+	        featureType: 'water',
+	        stylers: [{color: '#5fade7'}]
+	      }
+	    ], {
+	      name: 'Custom Style'
+	});
+
+  	var customMapTypeId = 'custom_style';
+
     var mapOptions = {
+        
         center: new google.maps.LatLng(43.705342, -72.288601),
         zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+        mapTypeControlOptions: {
+      		mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+    	}
     }
+
+    var mapCanvas = document.getElementById('map');
+
     var map = new google.maps.Map(mapCanvas, mapOptions)
+
+    map.mapTypes.set(customMapTypeId, customMapType);
+	map.setMapTypeId(customMapTypeId);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
