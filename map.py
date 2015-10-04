d0665ee2ea5344fe24b3d97efc552e52ea842d11
Russell Parker <russell.parker7@gmail.com>
2015-10-03 23:45:07 -0400

    Some changes + we made a new url function

--- map.py
+++ map.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask.ext.googlemaps import GoogleMaps
 from flask.ext.googlemaps import Map
+import json
 
 app = Flask(__name__, template_folder=".")
 GoogleMaps(app)
@@ -8,6 +9,11 @@ GoogleMaps(app)
 @app.route('/')
 def mapview():
     return render_template('map.html')
+
+@app.route('/hello')
+def hello():
+    data = {"data": "a"}
+    return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
