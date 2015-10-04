60a59bfb350150146b1b1498986284c5f7f24068
Russell Parker <russell.parker7@gmail.com>
2015-10-04 08:22:46 -0400

    Revamped initmap to center on user's location

--- static/js/initmap.js
+++ static/js/initmap.js
@@ -1,10 +1,66 @@
-function initialize() {
+window.onload = function() {
     var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-        center: new google.maps.LatLng(43.705342, -72.288601),
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    var startPos;
+    var geoOptions = {
+        timeout:        10 * 1000,
+        maximumAge: 5 * 60 * 1000,
     }
-    var map = new google.maps.Map(mapCanvas, mapOptions)
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+
+    var geoSuccess = function(position) {
+        startPos = position;
+        //document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+        //document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+        //console.log(position.coords.latitude,position.coords.longitude);
+        //ajax post
+        var mapOptions = {
+            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+    };
+    var geoError = function(error) {
+        console.log('Error occurred. Error code: ' + error.code);
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+};
+
+//google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+// window.onload = function() {
+//   var startPos;
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   navigator.geolocation.getCurrentPosition(geoSuccess);
+// };
+
+
+// <script type=text/javascript>
+//   $(function() {
+//     $('a#calculate').bind('click', function() {
+//       $.getJSON($SCRIPT_ROOT + '/_add_numbers', {
+//         a: $('input[name="a"]').val(),
+//         b: $('input[name="b"]').val()
+//       }, function(data) {
+//         $("#result").text(data.result);
+//       });
+//       return false;
+//     });
+//   });
+// </script>
+// <h1>jQuery Example</h1>
+// <p><input type=text size=5 name=a> +
+//    <input type=text size=5 name=b> =
+//    <span id=result>?</span>
+// <p><a href=# id=calculate>calculate server side</a>
