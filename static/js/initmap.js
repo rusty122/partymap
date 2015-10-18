a336404081efc0118ffdca3173d40c8d850e71d5
Derick Yang <derick.yang@tufts.edu>
2015-10-18 00:20:59 -0400

    =Add pin to current location. TODO: database.

--- static/js/initmap.js
+++ static/js/initmap.js
@@ -1,9 +1,8 @@
 var map = null;
 
 var people = [
-// test people right now, to populate with database info
+// test people right now, to use database later
     {lat: 43.705342, lng: -72.288601},
- //   {lat: 42, lng: -70}
     {lat: 42.407857, lng: -71.1209772}
 ];
 
@@ -55,8 +54,6 @@ function initialize() {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-
 function setMarkers(map) {
 //    var image = {
 //        url: 'static/img/MapPinNormal.png',
@@ -67,45 +64,39 @@ function setMarkers(map) {
     console.log("hello");
     console.log(people);
     for (var i = 0; i < people.length; i++) {
-        // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-
-        var marker = new google.maps.Marker({
-            icon: 'static/img/MapPinNormal3.png',
-            position: people[i],
-            map: map,
-         });
-        console.log(i);
-
+        
+        addLoc(map, people[i]);
     }
 
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function addLoc(map, person) {
+    var marker = new google.maps.Marker({
+    // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        icon: 'static/img/MapPinNormal3.png',
+        position: person,
+        map: map,
+     });
+}
 
 // Recenter the map to current location
 function recenterLoc(){
-	var geolocation = navigator.geolocation;
-	    if (navigator.geolocation) {
-	         navigator.geolocation.getCurrentPosition(function (position) {
-	             var currPos = {
-                lat: position.coords.latitude, 
-                lng: position.coords.longitude
-               };
-               console.log("dsafdf");
-               people.push(currPos);
-
-	             map.setCenter(currPos);
-	         });
-
-	    }
-
+  	var geolocation = navigator.geolocation;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                var currPos = {
+                  lat: position.coords.latitude, 
+                  lng: position.coords.longitude
+                };
+                people.push(currPos);
+                console.log(people);
+                map.setCenter(currPos);
+                addLoc(map, currPos);
+         });
+    }
 }
 
-/*
-function addLoc() {
-  var geolocation = navigator.geolocation
-}
-*/
 
 
 
